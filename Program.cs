6eaa95d784c35db2f744ae7ce4e2e0c8e230a1b9
Lerissa Lazar <lerissa.lazar12@gmail.com>
2022-10-21 10:16:47 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿//Lerissa Lazar
+//09-14-22
+//Reverse it
+//the user is asked to enter a number or word and the computer will reverse the input they give.
+
+Console.Clear();
+
+int num, remainder;
+int revNum = 0;
+int length = 0;
+bool validNum;
+bool loop = true;
+string input;
+string revStr = "";
+
+while (loop)
+{
+
+    Console.WriteLine("Enter a number or word: ");
+    input = Console.ReadLine();
+
+    validNum = int.TryParse(input, out num);
+
+    if (validNum)
+    {
+        revNum = 0;
+        while (num > 0)
+        {
+            remainder = num % 10;
+            revNum = (revNum * 10) + remainder;
+            num = num / 10;
+        }
+        Console.WriteLine(revNum);
+    }
+    else
+    {
+        revStr = "";
+        length = input.Length - 1;
+        while (length >= 0)
+        {
+            revStr = revStr + input[length];
+            length--;
+        }
+        Console.WriteLine(revStr);
+    }
+
+    Console.WriteLine("Do you want to try again? Yes or no?");
+    
+    string playAgain;
+    bool validInput = false;
+    while(!validInput)
+    {
+        playAgain = Console.ReadLine().ToLower();
+        validInput = true;
+        if(playAgain == "yes")
+        {
+            validInput = true;
+        }
+        else if(playAgain == "no")
+        {
+            validInput = true;
+            loop = false;
+            Console.WriteLine("Thank you for playing, bye!");
+        }
+        else
+        {
+            validInput = false;
+            Console.WriteLine("Invalid response, enter yes or no");
+        }
+    }
+}
